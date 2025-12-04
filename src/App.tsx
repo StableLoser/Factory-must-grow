00b663b37d23a9fcd856da65d3ecc9d523579d38
dell <Musa>
2025-12-04 13:17:17 +0100

    money

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import './App.css'
+import "./App.css";
 import MoneyShower from "./Components/MoneyShower.tsx";
-import {useState} from "react";
+import { useState } from "react";
 import Incremancer from "./Components/Incremancers.tsx";
 
-
 function App() {
-    const [money, setMoney] = useState<number>(0);
+  const [money, setMoney] = useState<number>(1000);
 
+  function collect(value: number) {
+    setMoney(money + value);
+  }
 
-    function collect(value: number) {
-        setMoney(money + value);
+  function cost(cost: number) {
+    if (money > 0) {
+      setMoney(money - cost);
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    return <div className={"container"}>
-
-
-        <MoneyShower className={"col money-shower text-center"} money={money}/>
-        <Incremancer onCollect={collect} name={"Iron Ore"}/>
-        <Incremancer onCollect={collect} name={"Iron Plate"}/>
-
-
-    </div>;
+  return (
+    <div className={"container"}>
+      <MoneyShower className={"col money-shower text-center"} money={money} />
+      <Incremancer onCollect={collect} onCost={cost} name={"Iron Ore"} />
+      <Incremancer onCollect={collect} onCost={cost} name={"Iron Plate"} />
+      <Incremancer onCollect={collect} onCost={cost} name={"Iron Gear"} />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/Components/Incremancers.tsx
+++ src/Components/Incremancers.tsx
@@ -1,49 +1,79 @@
 import UpgradeButton from "./UpgradeButton.tsx";
-
 import CollectButton from "./CollectButton.tsx";
-import {useState} from "react";
+import { useEffect, useState } from "react";
 
-const defaultSpeed = 10000;
+const defaultSpeed = 1000;
+const ironOreCost = 1;
 
-function Incremancer({name, onCollect, className}: {
-    name: string,
-    onCollect: (p1: number) => void,
-    className?: string
+function Incremancer({
+  name,
+  onCollect,
+  onCost,
+  className,
+}: {
+  name: string;
+  onCollect: (p1: number) => void;
+  onCost: (p1: number) => boolean;
+  className?: string;
 }) {
-    const [speed, setSpeed] = useState(defaultSpeed);
-    const [count, setCount] = useState(0);
+  const [speed, setSpeed] = useState(defaultSpeed);
+  const [count, setCount] = useState(0);
 
+  function upgrade() {
+    setSpeed(speed / 2);
+  }
 
-    function upgrade() {
-        setSpeed(speed / 2);
+  function increment() {
+    setCount((prevCount) => prevCount + 1);
+  }
+
+  function collect() {
+    onCollect(count);
+    setCount(0);
+  }
+
+  useEffect(() => {
+    if (onCost(ironOreCost)) {
+      const timer = setTimeout(() => {
+        increment();
+      }, speed);
+
+      return () => clearTimeout(timer);
     }
+  }, [speed, count]);
 
-    function increment() {
-        setCount(count + 1);
-    }
+  return (
+    <div className={"container " + className}>
+      <div className={"row"}>
+        <output className={"m-2 p-2 col col-md-auto"}>
+          {" "}
+          You have {count} {name}
+        </output>
+        <CollectButton className={" col col-md-auto"} onClick={collect} />
+      </div>
 
-    function collect() {
-        onCollect(count);
-        setCount(0);
-    }
-
-    setTimeout(increment, speed);
-
-    return <div className={"container " + className}>
-        <div className={"row"}>
-            <output className={"m-2 p-2 col col-md-auto"}> You have {count} {name}</output>
-            <CollectButton className={" col col-md-auto"} onClick={collect}/>
-        </div>
-
-        <div className={"row  "}>
-            <UpgradeButton name={"Faster"} upgradeCost={10} className={"col col-md-auto "} onClick={upgrade}/>
-            <UpgradeButton name={"Stronger"} upgradeCost={10} className={"col col-md-auto "} onClick={upgrade}/>
-            <UpgradeButton name={"Better"} upgradeCost={10} className={"col col-md-auto "} onClick={upgrade}/>
-        </div>
-
-
+      <div className={"row  "}>
+        <UpgradeButton
+          name={"Faster"}
+          upgradeCost={10}
+          className={"col col-md-auto "}
+          onClick={upgrade}
+        />
+        <UpgradeButton
+          name={"Stronger"}
+          upgradeCost={10}
+          className={"col col-md-auto "}
+          onClick={upgrade}
+        />
+        <UpgradeButton
+          name={"Better"}
+          upgradeCost={10}
+          className={"col col-md-auto "}
+          onClick={upgrade}
+        />
+      </div>
     </div>
-
+  );
 }
 
 export default Incremancer;
